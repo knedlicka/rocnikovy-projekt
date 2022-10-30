f7df46634ff445310432283d2709dca078d69e50
Nadiya Balanchuk <balanchuknadiy4@gmail.com>
2022-10-30 20:17:30 +0100

    Add function for generating int combinations
    * Initial implementation of combinations.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
+#include <vector>
+#include <set>
+#include <algorithm>
 #include <iostream>
 
+std::set<std::set<int>> combinations(std::set<int> s, int sz) {
+    int n = s.size();
+    std::vector<int> indexed_set(n);
+    std::copy(s.begin(), s.end(), indexed_set.begin());
+    std::vector<int> combs(n, 0);
+
+    for (int i = n - 1; i >= n - sz; i--) {
+        combs[i] = 1;
+    }
+
+    std::set<std::set<int>> ans;
+    do {
+        std::set<int> inst;
+        for (int i = 0; i < n; i++) {
+            if (combs[i]) {
+                inst.insert(indexed_set[i]);
+            }
+        }
+        ans.insert(inst);
+    } while (std::next_permutation(combs.begin(), combs.end()));
+
+    return ans;
+}
+
+void print_combinations(std::set<int> s, int sz){
+    std::set<std::set<int>> combs = combinations(s, sz);
+    for (auto x: combs) {
+        std::cout << "{ ";
+        for (auto y: x) {
+            std::cout << y << " ";
+        }
+        std::cout << "}\n";
+    }
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    print_combinations({1, 2}, 1);
+
     return 0;
 }
