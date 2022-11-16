8d86ebf5eb8d518de1f63adac1a61c84b0da1c06
Nadiya Balanchuk <balanchuknadiy4@gmail.com>
2022-11-16 21:02:03 +0100

    Add test for combinations without repetitions

--- Tests.h
+++ Tests.h
@@ -1,16 +1,30 @@
-//
-// Created by prasiatko on 11/16/22.
-//
-
 #ifndef RP_TESTS_H
 #define RP_TESTS_H
 
+#include "Combinatorics.h"
 
 class Tests {
-    int dummyFunc(int x, int y);
-    void testCombinationsWithoutRepetition();
-    void testPermutationsShort();
-    void testPermutationsLong();
+    static void testCombinationsWithoutRepetition() {
+        std::set<int> s = {1, 2, 3};
+        int k = 2;
+        std::set<std::set<int>> expectedCombinations = {
+                {1, 2},
+                {1, 3},
+                {2, 3}
+        };
+        auto calculatedCombinations = Combinatorics::combinations_without_repetitions(s, k);
+    if(calculatedCombinations != expectedCombinations) {
+        std::cout << "[FAIL] ";
+    } else {
+        std::cout << "[PASS] ";
+    }
+    const std::string test_name = "Combinations without repetitions, int, n=3, k=2";
+    std::cout << test_name << '\n';
+    }
+public:
+    static void runTests() {
+        testCombinationsWithoutRepetition();
+    }
 };
 
 
